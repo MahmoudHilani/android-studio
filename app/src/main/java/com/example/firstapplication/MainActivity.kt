867f3e4047b2227efb8de3b521730eb118705ca1
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2025-09-26 10:24:57 +0100

    Column, Row and Button

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,28 +1,44 @@
 package com.example.firstapplication
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TextCard("check the logcat window")
+            Column {
+                TextCard("check the logcat window")
+                TextCard("dont check the logcat window")
+                TextCard("i like to move it move it")
+                TextCard("will anyone ever read this")
+                Button(onClick = {
+                    if(buttonText.value == "off")
+                        buttonText.value = "on"
+                    else
+                        buttonText.value = "off"
+                }) {
+                    Text(buttonText.value)
+                }
+            }
+            Row {
+                TextCard("check the logcat window")
+                TextCard("dont check the logcat window")
+                TextCard("i like to move it move it")
+                TextCard("will anyone ever read this")
+            }
         }
-
-        Log.i("logcat test", "information message")
-        Log.v("logcat test", "verbose message")
-        Log.w("logcat test", "warning message")
-        Log.e("logcat test", "error message")
-        Log.d("logcat test", "debug message")
-        Log.wtf("logcat test", "something went rly bad message")
     }
+    var buttonText = mutableStateOf("off")
 }
 
 @Composable
